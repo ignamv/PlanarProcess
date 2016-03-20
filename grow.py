3e53a3c5b924c797a798bebfc9eef6cc676fc766
Ignacio Martinez Vazquez <ignamv@gmail.com>
2016-03-19 22:56:03 -0300

    Group actions and global wafer variables into Wafer class

--- grow.py
+++ grow.py
@@ -51,116 +51,128 @@ def ellipse(x, y, width, height, theta0, theta1, nvertices):
              y + height * numpy.sin(theta))
              for theta in numpy.linspace(theta0, theta1, nvertices)]
 
-maxy = 4
-air = GeometryReference(shapely.geometry.box(-2, 1, 4, maxy))
-solids = []
+class Wafer(object):
+    def __init__(self, substrate_height, maximum_y, minimum_x, maximum_x):
+        self.air = GeometryReference(shapely.geometry.box(minimum_x,
+            substrate_height, maximum_y, maximum_x))
+        self.substrate = GeometryReference(shapely.geometry.box(minimum_x, 0, 
+            maximum_x, substrate_height))
+        self.solids = [self.substrate]
 
-def grow(height, mask, base=None, consuming=None, outdiffusion=0., 
-        etching=False, outdiffusion_vertices=16, y_offset=0.):
-    '''Deposit height of material on regions specified by mask.
+    def grow(self, height, mask, base=None, consuming=None, outdiffusion=0., 
+            etching=False, outdiffusion_vertices=16, y_offset=0.):
+        '''Deposit height of material on regions specified by mask.
 
-    base is one or several GeometryReference, over which material is
-    deposited. By default, deposit over all solids.
-    consuming is one or several GeometryReference, which material can
-    grow into. By default, only grow into air.
-    outdiffusion is the width of the rounded border, interpolated with 
-    outdiffusion_vertices vertices.
-    y_offset shifts in the y axis, enabling partially buried growth (for
-    example oxidation consuming silicon) and floating structures.
-    '''
-    if base is None:
-        base = solids
-    if consuming is None:
-        consuming = air
-    base = ensure_list(base)
-    consuming = ensure_list(consuming)
-    if y_offset != 0 and numpy.sign(y_offset) != numpy.sign(y_offset + height):
-        # Partially buried, return union of implant and growth
-        base_copy = [b for b in base]
-        bottom = grow(y_offset, mask, base=consuming,
-                consuming=base, outdiffusion=outdiffusion, etching=etching,
-                outdiffusion_vertices=outdiffusion_vertices)
-        top = grow(height + y_offset, mask, base=base_copy + [bottom],
-                consuming=consuming, outdiffusion=outdiffusion,
-                etching=etching, outdiffusion_vertices=outdiffusion_vertices)
-        return GeometryReference(top.geometry.union(bottom.geometry))
-    base_union = shapely.ops.cascaded_union([b.geometry for b in base])
-    consuming_union = shapely.ops.cascaded_union([c.geometry for c in consuming])
-    whole_interface = base_union.intersection(consuming_union)
-    buried = numpy.sign(height) != numpy.sign(y_offset)
-    logger.debug('Growing %f on %s, consuming %s, mask %s, interface %s', 
-            height, base_union, consuming_union, mask, whole_interface)
-    polygons = []
-    for segment in mask:
-         interface = ensure_multilinestring(whole_interface.intersection(
-                 shapely.geometry.box(segment[0], 0, segment[1], maxy)))
-         for linestring in interface:
-             if not isinstance(linestring, shapely.geometry.LineString):
-                 # Don't want Points and other strange bits of the intersection
-                 continue
-             coords = [(x, y + y_offset) for x,y in linestring.coords]
-             top = [(x, y + height) for x,y in reversed(coords)]
-             polygon = shapely.geometry.Polygon(coords + top)
-             if polygon.area == 0:
-                 continue
-             polygons.append(polygon)
-             # Add lateral outgrowth/outdiffusion
-             if outdiffusion != 0.:
-                 updown = numpy.sign(height)
-                 large_y = 20
-                 for x, y in top:
-                     if y_offset == 0.:
-                         # Outdiffusion is a corner and a pillar to the 
-                         # base
-                         polygons.append(shapely.geometry.Polygon(
-                             ellipse(x, y - height,
-                                 outdiffusion, height,
-                                 0, numpy.pi, outdiffusion_vertices)
-                             + [(x - outdiffusion, y - updown * large_y),
-                             (x + outdiffusion, y - updown * large_y)]
-                         ))
-                     else:
-                         # Outdiffusion is a half ellipse
-                         polygons.append(shapely.geometry.Polygon(
-                             ellipse(x, y - height / 2,
-                                 outdiffusion, height / 2,
-                                 0, 2 * numpy.pi, outdiffusion_vertices)))
+        base is one or several GeometryReference, over which material is
+        deposited. By default, deposit over all solids.
+        consuming is one or several GeometryReference, which material can
+        grow into. By default, only grow into air.
+        outdiffusion is the width of the rounded border, interpolated with 
+        outdiffusion_vertices vertices.
+        y_offset shifts in the y axis, enabling partially buried growth (for
+        example oxidation consuming silicon) and floating structures.
+        '''
+        if base is None:
+            base = self.solids
+        if consuming is None:
+            consuming = self.air
+        base = ensure_list(base)
+        consuming = ensure_list(consuming)
+        if y_offset != 0 and numpy.sign(y_offset) != numpy.sign(y_offset 
+                + height):
+            # Partially buried, return union of implant and growth
+            base_copy = [b for b in base]
+            bottom = self.grow(y_offset, mask, base=consuming,
+                    consuming=base, outdiffusion=outdiffusion, etching=etching,
+                    outdiffusion_vertices=outdiffusion_vertices)
+            top = self.grow(height + y_offset, mask, base=base_copy + [bottom],
+                    consuming=consuming, outdiffusion=outdiffusion,
+                    etching=etching, 
+                    outdiffusion_vertices=outdiffusion_vertices)
+            return GeometryReference(top.geometry.union(bottom.geometry))
+        base_union = shapely.ops.cascaded_union([b.geometry for b in base])
+        consuming_union = shapely.ops.cascaded_union(
+                [c.geometry for c in consuming])
+        whole_interface = base_union.intersection(consuming_union)
+        buried = numpy.sign(height) != numpy.sign(y_offset)
+        logger.debug('Growing %f on %s, consuming %s, mask %s, interface %s', 
+                height, base_union, consuming_union, mask, whole_interface)
+        polygons = []
+        for segment in mask:
+             interface = ensure_multilinestring(whole_interface.intersection(
+                     shapely.geometry.box(segment[0], 0, segment[1],
+                         self.air.geometry.bounds[3])))
+             for linestring in interface:
+                 if not isinstance(linestring, shapely.geometry.LineString):
+                     # Don't want Points and other strange bits of the 
+                     # intersection
+                     continue
+                 coords = [(x, y + y_offset) for x,y in linestring.coords]
+                 top = [(x, y + height) for x,y in reversed(coords)]
+                 polygon = shapely.geometry.Polygon(coords + top)
+                 if polygon.area == 0:
+                     continue
+                 polygons.append(polygon)
+                 # Add lateral outgrowth/outdiffusion
+                 if outdiffusion != 0.:
+                     updown = numpy.sign(height)
+                     large_y = 20
+                     for x, y in top:
+                         if y_offset == 0.:
+                             # Outdiffusion is a corner and a pillar to the 
+                             # base
+                             polygons.append(shapely.geometry.Polygon(
+                                 ellipse(x, y - height,
+                                     outdiffusion, height,
+                                     0, numpy.pi, outdiffusion_vertices)
+                                 + [(x - outdiffusion, y - updown * large_y),
+                                 (x + outdiffusion, y - updown * large_y)]
+                             ))
+                         else:
+                             # Outdiffusion is a half ellipse
+                             polygons.append(shapely.geometry.Polygon(
+                                 ellipse(x, y - height / 2,
+                                     outdiffusion, height / 2,
+                                     0, 2 * numpy.pi, outdiffusion_vertices)))
 
-     # Only consume from specified geometry
-    ret = shapely.ops.cascaded_union(polygons).intersection(consuming_union)
-    for c in consuming:
-        c.geometry = c.geometry.difference(ret)
-    for b in base:
-        b.geometry = b.geometry.difference(ret)
-    ret = GeometryReference(ret)
-    if not etching:
-        solids.append(ret)
-    return ret
+         # Only consume from specified geometry
+        ret = shapely.ops.cascaded_union(polygons).intersection(
+                consuming_union)
+        for c in consuming:
+            c.geometry = c.geometry.difference(ret)
+        for b in base:
+            b.geometry = b.geometry.difference(ret)
+        ret = GeometryReference(ret)
+        if not etching:
+            self.solids.append(ret)
+        return ret
 
-def etch(depth, mask, consuming=None, **kwargs):
-    '''Etch depth on regions specified by mask.
+    def etch(self, depth, mask, consuming=None, **kwargs):
+        '''Etch depth on regions specified by mask.
 
-    consuming is one or several GeometryReference, which may be etched.
-    By default, etch any solid.
-    Additional kwargs are explained in grow() documentation.
-    '''
-    if consuming is None:
-        consuming = solids
-    hole = grow(-depth, mask, base=air, consuming=consuming, etching=True,
-            **kwargs)
-    air.geometry = air.geometry.union(hole.geometry)
+        consuming is one or several GeometryReference, which may be etched.
+        By default, etch any solid.
+        Additional kwargs are explained in grow() documentation.
+        '''
+        if consuming is None:
+            consuming = self.solids
+        hole = self.grow(-depth, mask, base=self.air, consuming=consuming,
+                etching=True, **kwargs)
+        self.air.geometry = self.air.geometry.union(hole.geometry)
 
-def implant(depth, mask, target=None, source=None, buried=0., **kwargs):
-    '''Implant to depth where specified by mask.
+    def implant(self, depth, mask, target=None, source=None, buried=0.,
+            **kwargs):
+        '''Implant to depth where specified by mask.
 
-    target is one or several GeometryReference, which may be implanted.
-    By default, implant any solid.
-    buried specifies the depth of the top edge of the implant (0 by default).
-    Additional kwargs are explained in grow() documentation.
-    '''
-    if target is None:
-        target = solids
-    if source is None:
-        source = air
-    return grow(-depth, mask, base=source, consuming=target,
-            y_offset=-buried, **kwargs)
+        target is one or several GeometryReference, which may be implanted.
+        By default, implant any solid.
+        buried specifies the depth of the top edge of the implant (0 by 
+        default).
+        Additional kwargs are explained in grow() documentation.
+        '''
+        if target is None:
+            target = self.solids
+        if source is None:
+            source = self.air
+        return self.grow(-depth, mask, base=source, consuming=target,
+                y_offset=-buried, **kwargs)
--- test.py
+++ test.py
@@ -1,28 +1,20 @@
+from grow import *
 
-#logger.setLevel(logging.DEBUG)
-#logger.addHandler(logging.StreamHandler())
-substrate = GeometryReference(shapely.geometry.box(-2, 0, 4, 1))
-solids.append(substrate)
-nplus = implant(.3, [(0,2)], outdiffusion=.1)
-nburied = implant(.3, [(-1,3)], buried=.4, outdiffusion=.1)
-m1 = grow(.2, [(-1.5, 0.5)], outdiffusion=.1)
-m2 = grow(.3, [(-1., 1.)], outdiffusion=.1)
-etch(1., [(-.25, .15)], outdiffusion=.2, outdiffusion_vertices=32)
-fox = grow(.2, [(-.5, 1.5)], outdiffusion=.1, y_offset=-.05)
-#fox_mask = [(-.5, 1.5)]
-#fox = implant(.05, fox_mask, outdiffusion=.1)
-#fox.geometry = fox.geometry.union(
-        #grow(.1, fox_mask, outdiffusion=.1).geometry)
+wafer = Wafer(1., 3., -2, 2)
+nplus = wafer.implant(.3, [(0,2)], outdiffusion=.1)
+nburied = wafer.implant(.3, [(-1,3)], buried=.4, outdiffusion=.1)
+m1 = wafer.grow(.2, [(-1.5, 0.5)], outdiffusion=.1)
+m2 = wafer.grow(.3, [(-1., 1.)], outdiffusion=.1)
+wafer.etch(1., [(-.25, .15)], outdiffusion=.2, outdiffusion_vertices=32)
+fox = wafer.grow(.2, [(-.5, 1.5)], outdiffusion=.1, y_offset=-.05)
 
 plot_geometryref(nburied, hatch='\\\\', fill=False, color='c')
 plot_geometryref(nplus, hatch='\\', fill=False, color='r')
 plot_geometryref(m1, hatch='//', fill=False, color='b')
 plot_geometryref(m2, hatch='//', fill=False, color='g')
 plot_geometryref(fox, fill=True, color=(.3, .3, .3))
-#plot_geometryref(fox1, fill=True, color=(.3, .3, .3))
-#plot_geometryref(fox2, fill=True, color=(.3, .3, .3))
-plot_geometryref(substrate, hatch='/', fill=False, zorder=-998)
-plot_geometryref(air, hatch='.', fill=False, linewidth=0, color=(.9,.9,.9),
+plot_geometryref(wafer.substrate, hatch='/', fill=False, zorder=-998)
+plot_geometryref(wafer.air, hatch='.', fill=False, linewidth=0, color=(.9,.9,.9),
         zorder=-999)
 pyplot.savefig('out.svg')
 pyplot.show()
